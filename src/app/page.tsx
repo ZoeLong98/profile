e05971fb015513496f9961721814f92f91c52e6d
Zoe Long <zoelong@Zoes-MacBook-Pro.local>
2024-09-28 10:37:38 -0700

    upload

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,151 @@
+"use client";
+import Link from "next/link";
 import Image from "next/image";
+import { Patrick_Hand_SC, Inria_Sans } from "next/font/google";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faGithub,
+  faInstagram,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-solid-svg-icons";
+import { motion, px } from "framer-motion";
+import { useState } from "react";
 
-export default function Home() {
+const patrickHandSc = Patrick_Hand_SC({
+  weight: "400",
+  subsets: ["latin"],
+});
+const Inria = Inria_Sans({
+  weight: "400",
+  subsets: ["latin"],
+});
+
+const FlowerAnimation = ({ isVisible }: { isVisible: boolean }) => {
+  const flowers = ["🌸", "🌼", "🌷", "🌹", "💖", "❤️"];
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <div className="absolute inset-0 translate-y-[-40px] translate-x-[-30px]">
+      {isVisible &&
+        flowers.map((flower, index) => (
+          <motion.div
+            key={index}
+            initial={{ opacity: 0, y: -50 }}
+            animate={{ opacity: 1, y: [0, -50, 0] }}
+            transition={{
+              duration: 2,
+              delay: index * 0.5,
+              repeat: 1,
+              yoyo: true,
+            }}
+            className="absolute text-2xl"
+            style={{
+              top: `${Math.random() * 100}%`,
+              left: `${Math.random() * 100}%`,
+            }}
+          >
+            {flower}
+          </motion.div>
+        ))}
+    </div>
+  );
+};
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+export default function PersonalInformation() {
+  const [isVisible, setIsVisible] = useState(false);
+  const [isDialogVisible, setIsDialogVisible] = useState(false);
+  const [isAboutVisible, setIsAboutVisible] = useState(false);
+
+  const toggleFlowers = () => {
+    setIsVisible(true);
+    setTimeout(() => {
+      setIsVisible(false);
+    }, 4000);
+  };
+  const toggleDialog = () => {
+    setIsDialogVisible(!isDialogVisible);
+  };
+  const toggleAbout = () => {
+    setIsAboutVisible(!isAboutVisible);
+  };
+
+  return (
+    <div className="bg-gray-100 flex flex-col items-center justify-center h-screen">
+      <div className="relative">
+        <FlowerAnimation isVisible={isVisible} />
+        <Image
+          className="absolute translate-y-[-130px]"
+          src={"/dialog.png"}
+          width={200}
+          height={200}
+          alt=""
+        ></Image>
+        <Image
+          src={"/images/me.jpg"}
+          width={200}
+          height={200}
+          className="rounded-full"
+          alt=""
+        ></Image>
+      </div>
+
+      <h1 className={`${patrickHandSc.className} text-5xl font-bold mt-3 z-10`}>
+        Zoe Long
+      </h1>
+      <p className={`${Inria.className} my-2`}>Hello World!</p>
+      <div className="flex flex-row justify-between w-30 mb-5">
+        {/* <FontAwesomeIcon icon={faInstagram} width={40} height={40} /> */}
+
+        <a href="https://github.com/ZoeLong98">
+          <FontAwesomeIcon icon={faGithub} width={40} />
+        </a>
+
+        {/* <FontAwesomeIcon icon={faLinkedin} width={40} /> */}
+        <div onClick={toggleDialog}>
+          <FontAwesomeIcon icon={faEnvelope} width={40} />
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+
+        {isDialogVisible && (
+          <div>
+            <Image
+              className="absolute translate-y-[-130px]"
+              src={"/dialog.png"}
+              width={200}
+              height={200}
+              alt=""
+            ></Image>
+            <div
+              className={`${Inria.className} absolute text-[10px] text-center translate-x-7 translate-y-[-55px]`}
+            >
+              <b>Email:</b>
+              <br />
+              zeyulong0908@gmail.com
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="flex justify-between w-40">
+        {isAboutVisible && (
+          <div
+            className={`${Inria.className} absolute border-4 border-black translate-x-[-150px] translate-y-[-50px] px-5 py-3 rounded-lg`}
+          >
+            <b>My position</b>
+            <p>front end</p>
+          </div>
+        )}
+
+        <button
+          onClick={toggleAbout}
+          className={`${Inria.className} bg-gray-300 py-1 px-2 rounded`}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          About
+        </button>
+        <button
+          className={`${Inria.className} bg-gray-300 py-1 px-2 rounded`}
+          onClick={toggleFlowers}
         >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+          Click me
+        </button>
+      </div>
     </div>
   );
 }
